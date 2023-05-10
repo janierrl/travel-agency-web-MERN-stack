f811a674f0ef94e4067e6670a49694d4324bcf7f
EdBernal00 <edbernal00@gmail.com>
2023-05-10 16:33:49 -0400

    add http server with express

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+// Import Routings
+// ...
+
+// Configure Body Parse
+// ...
+
+// Configure Header HTTP - CORS
+// ...
+
+// Configure Routings
+// ...
+
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 const { IP_SERVER, DB_NAME } = require("./constants");
 
 const PORT = process.env.POST || 27017;
@@ -6,6 +7,11 @@ const PORT = process.env.POST || 27017;
 mongoose.connect(`mongodb://${IP_SERVER}:${PORT}/${DB_NAME}`, (error) => {
   if (error) throw error;
   else {
-    console.log("Conexion exitosa");
+    app.listen(PORT, () => {
+        console.log("######################");
+        console.log("###### API REST ######");
+        console.log("######################");
+        console.log(`http://${IP_SERVER}:${PORT}`);
+      });
   }
 });
