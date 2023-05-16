98e6f08e23d9bfe6b5c108d2c6e30bcfce6e2293
Janier Rodríguez López <janierbits@yahoo.com>
2023-05-16 01:16:43 -0400

    delete unnecessary file

--- backend/src/router/test.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const testController = require("../controllers/test");      // Se importa el controlador de prueba que es la funcion del endpoint
-
-const router = Router();
-
-router.post("/test", testController.testFunction);     // Endpoint con la respectiva funcion
-
-module.exports = router;
